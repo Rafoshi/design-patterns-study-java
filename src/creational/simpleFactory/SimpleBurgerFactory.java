21be944f8f6544eca7fecadecd6b958b6a405286
Rafoshi <rafa.ioshi@gmail.com>
2025-01-16 00:52:37 -0300

    Annotation for simple factory

--- src/creational/simpleFactory/SimpleBurgerFactory.java
+++ src/creational/simpleFactory/SimpleBurgerFactory.java
@@ -4,6 +4,7 @@ public class SimpleBurgerFactory {
     public Burger createBurger(String request) {
         Burger burger = null;
 
+        //Open close principle broken
         if("BEEF".equals(request)) {
             burger = new BeefBurger();
         } else if("VEGGIE".equals(request)) {
